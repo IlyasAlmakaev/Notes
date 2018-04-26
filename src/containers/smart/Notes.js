cff5e56ec745b540b3d7e6690edd0fb2f19cfa1f
Ilyas <i.almakaev@gmail.com>
2018-04-26 14:00:47 +0700

    add methods

--- src/containers/smart/Notes.js
+++ src/containers/smart/Notes.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class Notes extends Component {
+class Note extends Component {
 
-  onAddNoteBtnClickHandler(e) {
-		e.preventDefault();
+  static propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape({
+      noteText: PropTypes.string.isRequired
+    })).isRequired,
+ }
 
+  static defaultProps = {
+    data: []
+  }
+  
+  constructor(props) {
+		super(props);
+		this.state = {noteText: this.props.data.noteText};
+			  
 	}
   onDeleteNoteBtnClickHandler(e) {
 		e.preventDefault();
@@ -13,24 +25,64 @@ export default class Notes extends Component {
   onCheckComplite(e) {
 	//	e.preventDefault();
 
+  }
+
+  render() {
+
+    return (
+    <form className='note'>
+    <h3>{this.state.noteText}</h3>
+    <label className='add__checkrule'>
+        <input type='checkbox' defaultCheked={false} ref='checkrule' onChange={this.onCheckComplite} />Выполнено
+    </label>
+    <button
+      className='add__btn'
+      onClick={this.onDeleteNoteBtnClickHandler}
+      ref='alert_button'>
+      Удалить заметку
+    </button>
+    </form>)
+  }
+}
+
+class NotesList extends Component {
+
+  render() {
+
+    let data = this.props.data;
+		let notesTemplate;
+
+		if (data.length > 0) {
+			notesTemplate = data.map(function(item, index) {
+				return (
+					<div key={index}>
+						<Note data={item} />
+					</div>
+				)
+			})
+		} else {
+			notesTemplate = <p>Заметок нет</p>
+		}
+
+    return (
+    <form className='note'>
+    {notesTemplate}
+    </form>)
+  }
+}
+
+export default class Notes extends Component {
+
+  onAddNoteBtnClickHandler(e) {
+		e.preventDefault();
+
 	}
 
   render() {
     return (
       <form className='notes'>
         <h1>Заметки</h1>
-        <form className='note'>
-          <h3>Первая заметка</h3>
-          <label className='add__checkrule'>
-							<input type='checkbox' defaultCheked={false} ref='checkrule' onChange={this.onCheckComplite} />Выполнено
-				  </label>
-          <button
-              className='add__btn'
-              onClick={this.onDeleteNoteBtnClickHandler}
-              ref='alert_button'>
-              Удалить заметку
-          </button>
-			  </form>
+        <Note />
         <button
           className='add__btn'
           onClick={this.onAddNoteBtnClickHandler}
